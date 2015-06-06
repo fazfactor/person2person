b4a72733c7cce198d2781a448a5b355a2b014378
amaldevsi <amaldevsitech@gmail.com>
2015-06-07 01:02:21 +0530

    Splash Screen with Loading Animation Home Page with Login with Google and Signup with Facebook

---
+++ Person2Person/src/com/angelhack/person2person/Home_Activity.java
@@ -0,0 +1,16 @@
+package com.angelhack.person2person;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Home_Activity extends  Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.home_activity);
+	}
+
+}
--- Person2Person/src/com/angelhack/person2person/MainActivity.java
+++ Person2Person/src/com/angelhack/person2person/MainActivity.java
@@ -1,36 +1,178 @@
-package com.angelhack.person2person;
+ package com.angelhack.person2person;
 
-import android.support.v7.app.ActionBarActivity;
+
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.angelhack.person2person.helperclass.Utils;
+
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.Window;
+import android.widget.ImageView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+	
+	
+	 @Override
+	    protected void onCreate(Bundle savedInstanceState) {
+		 this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		 setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+	        super.onCreate(savedInstanceState);  
+	        setContentView(R.layout.activity_main);
+	 
+	        new LoadViewTask().execute();
+	        
+	       
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	       
+	        
+	 }     
+	 //To use the AsyncTask, it must be subclassed  
+	      private class LoadViewTask extends AsyncTask<Void, Integer, Void>  
+	        {  
+	            //Before running code in separate thread  
+	            @Override  
+	            protected void onPreExecute()  
+	            {  
+	            	// prograss bar rotation in splash page
+	            	ImageView myAnimation = (ImageView)findViewById(R.id.myanimation);
+	     	        final AnimationDrawable myAnimationDrawable = (AnimationDrawable)myAnimation.getDrawable();
 
+	     	        myAnimation.post(
+	     	        new Runnable(){
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	     	          @Override
+	     	          public void run() {
+	     	           myAnimationDrawable.start();
+	     	          }
+	     	        });
+	     	       
+	            }  
+	      
+	            //The code to be executed in a background thread.  
+	            @Override  
+	            protected Void doInBackground(Void... params)  
+	            {  
+	            	
+	            		 if (Utils.isNetworkAvailable(MainActivity.this))
+	            		 {
+	 	                	
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	     	                /* This is just a code that delays the thread execution 4 times, 
+	     	                 * during 850 milliseconds and updates the current progress. This 
+	     	                 * is where the code that is going to be executed on a background 
+	     	                 * thread must be placed. 
+	     	                 */  
+	     	                try  
+	     	                {  
+	     	                    //Get the current thread's token  
+	     	                    synchronized (this)  
+	     	                    {  
+	     	                        //Initialize an integer (that will act as a counter) to zero  
+	     	                        int counter = 0;  
+	     	                        //While the counter is smaller than four  
+	     	                        while(counter <= 4)  
+	     	                        {  
+	     	                            //Wait 400 milliseconds  
+	     	                            this.wait(400);  
+	     	                            //Increment the counter  
+	     	                            counter++;  
+	     	                            //Set the current progress.  
+	     	                            //This value is going to be passed to the onProgressUpdate() method.  
+	     	                            publishProgress(counter*25);  
+	     	                        }  
+	     	                    }  
+	     	                }  
+	     	                catch (InterruptedException e)  
+	     	                {  
+	     	                    e.printStackTrace();  
+	     	                } 
+	     	                
+
+	     	  		
+	 		     	  		
+	 		     	  		
+	            	}else
+	            	{
+						
+					
+
+	                /* This is just a code that delays the thread execution 4 times, 
+	                 * during 600 milliseconds and updates the current progress. This 
+	                 * is where the code that is going to be executed on a background 
+	                 * thread must be placed. 
+	                 */  
+	                try  
+	                {  
+	                    //Get the current thread's token  
+	                    synchronized (this)  
+	                    {  
+	                        //Initialize an integer (that will act as a counter) to zero  
+	                        int counter = 0;  
+	                        //While the counter is smaller than four  
+	                        while(counter <= 4)  
+	                        {  
+	                            //Wait 850 milliseconds  
+	                            this.wait(600);  
+	                            //Increment the counter  
+	                            counter++;  
+	                            //Set the current progress.  
+	                            //This value is going to be passed to the onProgressUpdate() method.  
+	                            publishProgress(counter*25);  
+	                        }  
+	                    }  
+	                }  
+	                catch (InterruptedException e)  
+	                {  
+	                    e.printStackTrace();  
+	                } 
+	                
+                    
+	            	}
+		     	      
+	                return null;  
+	               
+	            }  
+	      
+	            //Update the progress  
+	            @Override  
+	            protected void onProgressUpdate(Integer... values)  
+	            {  
+	               
+	            }  
+	      
+	            //after executing the code in the thread  
+	            @Override  
+	            protected void onPostExecute(Void result)  
+	            {  
+	            	
+	            	Intent intent = new Intent(MainActivity.this,MainActivity.class);
+	    			startActivity(intent);
+	        		
+	        		finish();
+	            }  
+	        }  
+	      
+	  
+	      @Override
+	  	public void onPause() {
+	  		// TODO Auto-generated method stub
+	  		super.onPause();
+	  		
+	  	}
+	  	@Override
+	  	public void onDestroy()
+	  	{
+	  	        super.onDestroy();
+	  	}
 }
