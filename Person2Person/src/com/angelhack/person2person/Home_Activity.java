392dafe9da30afad695793befd3608287c7b3ecd
amaldevsi <amaldevsitech@gmail.com>
2015-06-07 01:58:21 +0530

    Start Work In Sign In with Google

--- Person2Person/src/com/angelhack/person2person/Home_Activity.java
+++ Person2Person/src/com/angelhack/person2person/Home_Activity.java
@@ -10,14 +10,18 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 
 public class Home_Activity extends  SherlockFragmentActivity {
 	
 	Typeface Roboto_bold,Roboto_light;
+	ImageView Signin_Gmail,
+			  Signup_Facebook;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +29,8 @@ public class Home_Activity extends  SherlockFragmentActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.home_activity);
 		
-		Roboto_light = Typeface.createFromAsset(getAssets(),"Roboto-Light.ttf"); 
-		Roboto_bold = Typeface.createFromAsset(getAssets(),"Roboto_bold.ttf"); 
+	//	Roboto_light = Typeface.createFromAsset(getAssets(),"Roboto-Light.ttf"); 
+	//	Roboto_bold = Typeface.createFromAsset(getAssets(),"Roboto-Bold.ttf"); 
 		
 		
 		// custom action bar layout
@@ -39,7 +43,7 @@ public class Home_Activity extends  SherlockFragmentActivity {
 		TextView title_head=(TextView)findViewById(R.id.content_title);
 	    title_head.setText("PERSON 2 PERSON");
 	    
-	    title_head.setTypeface(Roboto_bold,Typeface.BOLD);
+	 //   title_head.setTypeface(Roboto_bold,Typeface.BOLD);
 
 		// back action
 		RelativeLayout action_back = (RelativeLayout) findViewById(R.id.back_button);
@@ -54,6 +58,36 @@ public class Home_Activity extends  SherlockFragmentActivity {
 
 			}
 		});
+		
+		
+		
+		Signin_Gmail=(ImageView) findViewById(R.id.signin_gmail_iv_id);
+		Signup_Facebook=(ImageView) findViewById(R.id.signup_facebbok_iv_id);
+		
+		
+		Signin_Gmail.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				
+				
+				
+			}
+		});
+		
+		
+		Signup_Facebook.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+				Toast.makeText(Home_Activity.this, "Under Construction...!", Toast.LENGTH_SHORT).show();
+				
+			}
+		});
+	
 	}
 
 }
